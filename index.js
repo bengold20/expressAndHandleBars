3202eb42b3153b5ff7ff86a034c852abf12864f8
ben <phattrien0222@gmail.com>
2021-04-08 13:16:47 +0700

    sss

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
 
-        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpg'
-        // cb(null, file.fieldname + '-' + uniqueSuffix)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpg'
+        cb(null, file.fieldname + '-' + uniqueSuffix)
 
 
     }
