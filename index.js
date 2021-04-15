2bee533934c9253df3c8933875ea9189caff8129
ben <phattrien0222@gmail.com>
2021-04-15 23:20:52 +0700

    new1504

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const Handlebars = require("express-handlebars");
 //tạo app để cấu hình router
 var app = express();
 
+const mongoose = require('mongoose');
+async function connectdb(){
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.5xypv.mongodb.net/databaseben?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+    console.log("connect successfully!1")
+}
+
+connectdb();
+
+var user = new mongoose.Schema({
+    Name: String,
+    Gender: String,
+    Email: String,
+    Password: String,
+    Phone: String,
+    ImageURL : String
+})
+
+var userConnect = mongoose.model("users",user);
+
+
+
 //chạy lên localhost với port 3111
 app.listen(process.env.PORT || '3112');
 
@@ -47,21 +73,68 @@ app.post('/upload',uploadx, function (req, res) {
 
 app.engine('handlebars', Handlebars());
 app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'));
 
-app.get('/upload', function (req, res, next) {
-    res.render('upload', {title: 'Express'});
+app.get('/upload',async function (req, res, next) {
+    const userList  = await userConnect.find({});
+    res.render('upload', {title: 'Express', userList});
 });
 
-app.get('/',function (req, res){
+app.get('/',async function (req, res){
+    // const newuser = await userConnect.create({
+    //     userName: "String-userName",
+    //     password: "String-password",
+    //     name: "String-name",
+    //     address: "String-address",
+    //     numberPhone: "-numberPhone",
+    // });
+    // console.log(newuser);
     res.render("index")
 })
 
+app.post("/postdata", async (req,res, next)=>{
+    await userConnect.create(req.body);
+    res.redirect("/sigup");
+})
 
 app.get('/login',function (req,res) {
     res.render("login");
 })
-
-app.get('/sigup',function (req,res) {
-    res.render("sigup");
+app.get("/users", async (req, res) => {
+    let userList =await userConnect.find({});
+    userList = userList.map((item)=> item.toObject());
+    // res.status(200).json(userList);
+    res.render("upload", {
+        userList,
+        title : "users list",
+        isHasData : userList.length > 0 ? true : false });
 })
+app.route("/user/:id").get(async (req, res) => {
+    const user =(await userConnect.findById({_id : req.params.id})).toObject();
+    // res.status(200).json(user);
+    res.render("user", {user});
+}).post(uploadx,async (req,res)=>{
+    const data = {
+        ...req.body,
+        ImageURL : "/" + req.file.path.split("\\").slice(1,req.file.path.split("\\").length).join("/")}
+    const newUser = await   userConnect.findByIdAndUpdate({_id : req.params.id }, data);
+    res.redirect('/users')
+})
+
+app.get("/user/:id/delete", async (req, res) => {
+    await userConnect.findByIdAndDelete({_id : req.params.id});
+    res.redirect("back")
+    // res.render("upload", {userList});
+})
+app.route("/signup")
+    .get(function (req,res) {
+    res.render("signup");})
+    .post( uploadx, async function (req,res,next){
+        const data = {
+            ...req.body,
+            ImageURL : "/" + req.file.path.split("\\").slice(1,req.file.path.split("\\").length).join("/")}
+         const newUser = await   userConnect.create(data);
+        res.redirect('/users')
+    })
