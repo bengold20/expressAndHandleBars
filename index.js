3b0984feecc15af7cfcfacb5af8b0e3839ddbd36
ben <phattrien0222@gmail.com>
2021-04-20 16:44:52 +0700

    new1504

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var userConnect = mongoose.model("users", user);
 
 
 //chạy lên localhost với port 3111
-app.listen(process.env.PORT || '3112');
+app.listen(process.env.PORT || '3113');
 
 var multer = require('multer');
 var storage = multer.diskStorage({
@@ -57,18 +57,8 @@ var uploadx = multer({
 }).single('avatar')
 
 app.post('/upload', uploadx, function (req, res) {
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    // req.body will hold the text fields, if there were any
     console.log(req.file, req.body);
-
-    // (req, res, function (error) {
-    //     if (error instanceof multer.MulterError) {
-    //         return res.send("File size Maximum is 1MB.Please try again!!!")
-    //     } else {
-    //         return res.send('Loi ko xac dinh');
-    //     }
     res.send('Successfully!!')
-    // });
 });
 
 app.engine('handlebars', Handlebars());
@@ -77,10 +67,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 
-app.get('/upload', async function (req, res, next) {
-    const userList = await userConnect.find({});
-    res.render('upload', {title: 'Express', userList});
-});
+// app.get('/upload', async function (req, res, next) {
+//     const userList = await userConnect.find({});
+//
+//     res.render('upload', {title: 'Express', userList});
+// });
 
 app.get('/', async function (req, res) {
     res.render("index")
@@ -97,12 +88,16 @@ app.get('/login', function (req, res) {
 app.get("/userList", async (req, res) => {
     let userList = await userConnect.find({});
     userList = userList.map((item) => item.toObject());
-    // res.status(200).json(userList);
-    res.render("userList", {
-        userList,
-        title: "users list",
-        isHasData: userList.length > 0 ? true : false
+    res.json({
+        status : 200,
+        message : "ok",
+        data : userList
     });
+    // res.render("userList", {
+    //     userList,
+    //     title: "users list",
+    //     isHasData: userList.length > 0 ? true : false
+    // });
 })
 app.route("/user/:id").get(async (req, res) => {
     const user = (await userConnect.findById({_id: req.params.id})).toObject();
@@ -121,6 +116,13 @@ app.get("/user/:id/delete", async (req, res) => {
     await userConnect.findByIdAndDelete({_id: req.params.id});
     res.redirect("back")
 })
+
+
+router.get('/getUsers', async function (req, res) {
+    const userList = await userConnect.find({});
+    res.json(userList)
+})
+
 app.route("/signup")
     .get(function (req, res) {
         res.render("signup");
